bab74a66f7298d93e3741bc02af5a8732faf8a56
Abdallah-tech-cloud <sallablaye817.2009@gmail.com>
2026-03-06 07:25:41 +0000

    Update welcome message and add click event listener

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
-console.log("Bienvenue sur le site de Mohamed Djafar Abdalla Sall");
+console.log("Bienvenue sur le site officiel de Mohamed Djafar Abdalla Sall");
+
+document.querySelectorAll("a").forEach(link => {
+link.addEventListener("click", function(){
+console.log("Navigation sur le site");
+});
+});
